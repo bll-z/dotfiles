367ed6eb5b70931322d00958bf44a3d39965b669
Bill Zeligson <bll-z@users.noreply.github.com>
2015-05-02 15:43:42 -0400

    Update brew file

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -16,50 +16,17 @@ chflags nohidden $HOME/Library
 # copy the files
 # run the bootstrap
 ./bootstrap.sh
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
 # set bash4 and zsh to default terminal
 echo "/usr/local/bin/bash" | sudo tee -a /etc/shells
-echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/zsh
 
 # Create needed directories
 mkdir $TOOLS_HOME/lib
-mkdir -p $HOME/.pip/{cache,wheels} $VIRTUALENVS_HOME
-
-# Mongo
-ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
-# ElasticSearch
-ln -sfv /usr/local/opt/elasticsearch/*.plist ~/Library/LaunchAgents
-launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
-# Plus river plugin
-git clone git://github.com/richardwilly98/elasticsearch-river-mongodb.git /usr/local/var/lib/elasticsearch/plugins
-cd /usr/local/var/lib/elasticsearch/plugins/elasticsearch-river-mongodb
-./install-local.sh
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+mkdir -p $HOME/.pip/cache $VIRTUALENVS_HOME
 
 # MySql
 ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-# Postgres
-initdb -D /usr/local/pgsql/data
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-
-# MsSql - remove originals and symlink included
-# brew install freetds --with-unixodbc #0.91
-# rm -f /usr/local/Cellar/freetds/0.91/etc/freetds.conf
-# rm -f /usr/local/Cellar/unixodbc/2.3.2/etc/odbc.ini
-# rm -f /usr/local/Cellar/unixodbc/2.3.2/etc/odbcini.ini
-# rm -f /usr/local/etc/odbc.ini
-# rm -f /usr/local/etc/odbcini.ini
-# rm -f /usr/local/etc/freetds
-# ln -s ~/db_files/mssql/freetds.conf /usr/local/Cellar/freetds/0.91/etc
-# ln -s ~/db_files/mssql/freetds.conf /usr/local/etc
-# ln -s ~/db_files/mssql/odbc.ini /usr/local/etc
-# ln -s ~/db_files/mssql/odbc.ini /usr/local/Cellar/unixodbc/2.3.2/etc/
-# ln -s ~/db_files/mssql/odbcini.ini /usr/local/etc
-# ln -s ~/db_files/mssql/odbcini.ini /usr/local/Cellar/unixodbc/2.3.2/etc/
 
 # get rid of the init script
 rm -f ~/init_script.sh
